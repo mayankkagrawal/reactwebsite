47fc168aa99c9f0ca6fcba76e948b6c7c3c4cc62
mayankkagrawal <40241668+mayankkagrawal@users.noreply.github.com>
2021-01-28 22:34:30 +0530

    new

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from './Images/2.jpg';
+import app from './Images/3.jpg';
+import android from './Images/4.jpg';
+import digital from './Images/5.jpg';
+import marketig from './Images/6.jpg';
+import software from './Images/7.jpg';
+
+const Sdata =[
+    {
+        imgsrc: web,
+        title: "web development"
+    },
+    {
+        imgsrc: app,
+        title: "app development"
+    },
+    {
+        imgsrc: software,
+        title: "software development"
+    },
+    {
+        imgsrc: android,
+        title: "android development"
+    },
+    {
+        imgsrc: digital,
+        title: "digital development"
+    },
+    {
+        imgsrc: marketig,
+        title: "marketing development"
+    },
+
+];
+export default Sdata;
